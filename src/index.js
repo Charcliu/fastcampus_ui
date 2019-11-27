65d5bb5e131891cabf0a75d91f11159d993178dc
Wofluently <f_liuchang@163.com>
2019-11-27 17:18:06 +0800

    add description

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import TimeSelect from "./packages/TimeSelect/index";
 import Exception from "./packages/Exception/index";
 
 const components = [Banner, TimeSelect, Exception];
-const version = "0.1.19";
+const version = "0.1.20";
 
 const install = function(Vue) {
   console.log("全局安装fastcampus_ui组件");
